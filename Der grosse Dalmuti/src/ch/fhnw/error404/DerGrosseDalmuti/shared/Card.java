acb3e17d27cfef404b0cbb819c3552eeb66bb78e
Jonas Däster <schalk.lpd@gmail.com>
2013-11-01 01:02:51 +0100

    Created new Model-Classes Made simple tests. Does not work yet.

---
+++ Der grosse Dalmuti/src/ch/fhnw/error404/DerGrosseDalmuti/shared/Card.java
@@ -0,0 +1,12 @@
+package ch.fhnw.error404.DerGrosseDalmuti.shared;
+/**
+ * @author Jonas
+ *
+ */
+public class Card {
+
+	private String name;
+	private int value;
+	public enum CARD_TYPE{};
+	
+}
