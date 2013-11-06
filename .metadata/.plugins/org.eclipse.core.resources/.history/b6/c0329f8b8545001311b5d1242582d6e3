351c663f86b6cdc43b70abaffc2399afbbd8cf5e
Jonas Däster <schalk.lpd@gmail.com>
2013-11-06 13:18:02 +0100

    few little changes
    nothing to mention

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/b6/c0329f8b8545001311b5d1242582d6e3
@@ -0,0 +1,14 @@
+package ch.fhnw.error404.DerGrosseDalmuti.shared;
+
+import java.util.List;
+/**
+ * @author Jonas
+ *
+ */
+public class Deck {
+	
+	private List<Card> currentRound;
+	private List<Card> allPlayedCards;
+	private List<Card>[] playerCards;
+	
+}
--- Der grosse Dalmuti/src/ch/fhnw/error404/DerGrosseDalmuti/client/Client.java
+++ Der grosse Dalmuti/src/ch/fhnw/error404/DerGrosseDalmuti/client/Client.java
@@ -1,63 +1,61 @@
 package ch.fhnw.error404.DerGrosseDalmuti.client;
 
-
-
 import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 
-
 import ch.fhnw.error404.DerGrosseDalmuti.shared.*;
 
 public class Client {
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		Client client = new Client();
 		client.clientSocket();
+
 	}
-	
-	private void clientSocket(){
-		
+
+	private void clientSocket() {
+
 		String host = "127.0.0.1";
 		int port = 5000;
-		Player jonas = new Player("Jonas");
-		Object object = jonas;
-		
+
 		Socket socket = null;
-		
+
 		try {
-			
+
 			// create socket
-			socket = new Socket( host, port );
-			
+			socket = new Socket(host, port);
+			System.out.println("Zum Server verbunden: " + socket.isConnected());
+
 			// create outputStream for objects
 			OutputStream os = socket.getOutputStream();
-			ObjectOutputStream oos = new ObjectOutputStream( os );
-			
+			ObjectOutputStream oos = new ObjectOutputStream(os);
+
 			// write object to outputStream
-			oos.writeObject( object );
+			oos.writeObject(new Player ("Jonas"));
 			oos.flush();
-			
+
 			// create inputStream for objects
 			InputStream is = socket.getInputStream();
-			ObjectInputStream ois = new ObjectInputStream( is );
-			
+			ObjectInputStream ois = new ObjectInputStream(is);
+
 			// read object from inputStream
 			Object object2 = ois.readObject();
-			
+
 			// do something with object
-			Player anyPlayer = (Player)object2;
+			Player anyPlayer = (Player) object2;
 			System.out.println(anyPlayer.getRank());
-			
+
 			// clean up
 			oos.close();
 			ois.close();
-			
 		}
 
-		catch ( Exception e ) {
-		      e.printStackTrace();
+		catch (Exception e) {
+			e.printStackTrace();
+
 		}
+
 	}
 }
